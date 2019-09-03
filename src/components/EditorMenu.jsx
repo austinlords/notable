0807eb82c24212fd371f3e69224c97b1cd163d97
Austin Lords <austinlords@outlook.com>
2019-09-03 16:27:13 -0700

    small cleanup

--- src/components/EditorMenu.jsx
+++ src/components/EditorMenu.jsx
@@ -4,10 +4,6 @@ import { Link } from "react-router-dom";
 const EditorMenu = ({ save }) => {
   return (
     <div className="editor-menu">
-      <div className="clickable editor-menu-option">
-        <i className="fa fa-pencil-square-o" aria-hidden="true" />
-        <span> Edit</span>
-      </div>
       <Link to="/notes/new">
         <div className="clickable editor-menu-option">
           <i className="fa fa-plus" aria-hidden="true" />
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/js/dist/collapse";
 import "font-awesome/css/font-awesome.css";
@@ -18,4 +16,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
